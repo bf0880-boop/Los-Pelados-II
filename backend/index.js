95c14b745261294efe5cbeec3319ba26b1886c73
bf0880-boop <bf0880@est.ort.edu.ar>
2025-10-05 21:05:41 -0300

    Cambié algunas cosas

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,28 @@
-import{sucribeGETEvent,sucribePOSTEvent,realTimeEvent,startServer} from "soquetic";
+import { subscribeGETEvent, subscribePOSTEvent, startServer } from "soquetic";
 import fs from "fs";
-
-function enviarSabores(){
-    let sabores = JSON.parse(fs.readFileSync("data/sabores.json","utf-8"));
-    return sabores;
+function enviarSabores() {
+  const sabores = JSON.parse(fs.readFileSync("../data/sabores.json", "utf-8"));
+  return sabores;
 }
-function enviarProductos(){
-    let productos= JSON.parse(fs.readFileSync("data/productos.json","utf-8"));
-    return productos;
+function enviarProductos() {
+  const productos = JSON.parse(fs.readFileSync("../data/productos.json", "utf-8"));
+  return productos;
 }
-function enviarPedido(producto, sabores, nombre){
-    let pedidos = JSON.parse(fs.readFileSync("data/pedidos.json","utf-8"));
-    if(!Array.isArray(pedidos)) pedidos = [];
-    let nuevoPedido = {producto, sabores, nombre: nombre};
-    pedidos.push(nuevoPedido);
-    let pedidosJSON = JSON.stringify(pedidos, null, 2);
-    fs.writeFileSync("data/pedidos.json", pedidosJSON, "utf-8");
-    return {ok:true};
+function enviarPedido(req) {
+  let { producto, sabores, nombre } = req.data;
+  let pedidos = [];
+  try {
+    pedidos = JSON.parse(fs.readFileSync("../data/pedidos.json", "utf-8"));
+  } catch {
+    pedidos = [];
+  }
+  if (!Array.isArray(pedidos)) pedidos = [];
+  let nuevoPedido = { producto, sabores, nombre };
+  pedidos.push(nuevoPedido);
+  fs.writeFileSync("../data/pedidos.json", JSON.stringify(pedidos, null, 2));
+  return { ok: true };
 }
-sucribeGETEvent("sabores", enviarSabores);
-sucribeGETEvent("productos", enviarProductos);
-sucribePOSTEvent("pedido", enviarPedido);
-startServer(3000);
+subscribeGETEvent("sabores", enviarSabores);
+subscribeGETEvent("productos", enviarProductos);
+subscribePOSTEvent("pedido", enviarPedido);
+startServer();
