7a7f466e6f8361fde225cd226fe92e2070b33c2a
bf0880-boop <bf0880@est.ort.edu.ar>
2025-10-06 11:37:43 -0300

    Terminé, me funcionó

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,22 @@
 import { subscribeGETEvent, subscribePOSTEvent, startServer } from "soquetic";
 import fs from "fs";
 function enviarSabores() {
-  const sabores = JSON.parse(fs.readFileSync("../data/sabores.json", "utf-8"));
+  const sabores = JSON.parse(fs.readFileSync("data/sabores.json", "utf-8"));
   return sabores;
 }
 function enviarProductos() {
-  const productos = JSON.parse(fs.readFileSync("../data/productos.json", "utf-8"));
+  const productos = JSON.parse(fs.readFileSync("data/productos.json", "utf-8"));
   return productos;
 }
-function enviarPedido(req) {
-  let { producto, sabores, nombre } = req.data;
-  let pedidos = [];
-  try {
-    pedidos = JSON.parse(fs.readFileSync("../data/pedidos.json", "utf-8"));
-  } catch {
-    pedidos = [];
-  }
+function enviarPedido(producto, sabores, nombre) {
+  let pedidos = JSON.parse(fs.readFileSync("data/pedidos.json", "utf-8"));
+
   if (!Array.isArray(pedidos)) pedidos = [];
-  let nuevoPedido = { producto, sabores, nombre };
-  pedidos.push(nuevoPedido);
-  fs.writeFileSync("../data/pedidos.json", JSON.stringify(pedidos, null, 2));
-  return { ok: true };
+  let Pedido = {producto: producto, sabores: sabores, nombre: nombre};
+  pedidos.push(Pedido);
+  let pedidosJSON = JSON.stringify(pedidos, null, 2)
+  fs.writeFileSync("data/pedidos.json");
+  return {ok: true};
 }
 subscribeGETEvent("sabores", enviarSabores);
 subscribeGETEvent("productos", enviarProductos);
